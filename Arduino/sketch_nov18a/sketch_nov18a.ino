f2705f7e9cf968798d1fe6a8b0e8f51056dc981a
Sandris <sandrisburnovskis@gmail.com>
2020-12-17 07:04:40 +0200

    heater update

--- Arduino/sketch_nov18a/sketch_nov18a.ino
+++ Arduino/sketch_nov18a/sketch_nov18a.ino
@@ -1,67 +1,21 @@
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
-// Set DHT pin:
 #define DHTPIN 13
-// Set DHT type, uncomment whatever type you're using!
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DHTTYPE DHT22
+#define RELAYPIN 10
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 DHT dht = DHT(DHTPIN, DHTTYPE);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  dht.begin();
+    pinMode(RELAYPIN, OUTPUT);
+    digitalWrite(RELAYPIN, HIGH);
+    lcd.begin(16, 2);
+    dht.begin();
     Serial.begin(9600);
 }
 
@@ -69,12 +23,29 @@ void loop() {
 
     float h = dht.readHumidity();
     float t = dht.readTemperature();
-    Serial.print(t);  
     lcd.setCursor(0, 0);  
     lcd.print(h);
     lcd.print(" mitrums%  ");
     lcd.setCursor(0, 1);  
     lcd.print(t);
     lcd.print(" C     ");
-    delay(10000);
+
+    if (t > 0.00 && t <= 18.00)
+    {    
+
+
+        digitalWrite(RELAYPIN, LOW);
+
+        Serial.print(t); 
+        Serial.println(" C -> Heater on!"); 
+
+    }    
+    else
+    {
+
+        digitalWrite(RELAYPIN, HIGH); 
+        Serial.print(t); 
+        Serial.println(" C -> Heater off!"); 
+    }
+    delay(1 5000);  
 }
